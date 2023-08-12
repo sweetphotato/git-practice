e3387bf6f3b1970c6a1786857518c43139dee6b2
little fox <littlefoxgit@gmail.com>
2023-08-12 22:15:25 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# ch 4.2.1 main.py
+'''
+def check():
+    print("test")
+
+if __name__=='__main__':
+    check()
+
+'''
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+class Calculator(QWidget) :
+    
+    
+    def __init__(self) :
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle('Calculator')
+        self.resize(256,256)
+        self.show()
+        
+        
+if __name__=='__main__' :
+    app = QApplication(sys.argv)
+    view = Calculator()
+    sys.exit(app.exec_())
