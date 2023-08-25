47e9318b8901c7ebe1ae28d1e13af5c09671ff09
little fox <littlefoxgit@gmail.com>
2023-08-25 22:58:40 +0900

    Modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -7,13 +7,26 @@ class Control:
         self.connectSignals()
         
     def calculate(self):
-        num1 = float(self.view.le1.text())
-        num2 = float(self.view.le2.text())
-        operator = self.view.cb.currentText()
-        
-        if operator == '+' :
-            return f'{num1} + {num2} = {self.sum(num1,num2)}'
-        else :
+        try:
+            num1 = float(self.view.le1.text())
+            num2 = float(self.view.le2.text())
+            operator = self.view.cb.currentText()
+            
+            if operator == '+' :
+                return f'{num1} + {num2} = {self.sum(num1,num2)}'
+            elif operator == '-':
+                return f'{num1} - {num2} = {self.sub(num1,num2)}'
+            elif operator == '*':
+                return f'{num1} * {num2} = {self.mul(num1,num2)}'
+            elif operator == '/':
+                return f'{num1} / {num2} = {self.div(num1,num2)}'
+            elif operator == '^':
+                return f'{num1} ^ {num2} = {self.pow(num1,num2)}'
+            #elif operator == '%':
+            #    return f'{num1} % {num2} = {self.mod(num1,num2)}'
+            else:
+                return "Calculation Error"
+        except:
             return "Calculation Error"
         
     def connectSignals(self):
@@ -22,13 +35,6 @@ class Control:
  
     def sum(self, a,b) :
         return a+b
-    '''
-        try :
-            return str(a+b)
-        except:
-            return "Calculation Error"
-
-    '''
     
     def sub(self, a,b):
         return a-b
@@ -54,5 +60,14 @@ class Control:
             return e
   
         return pow(a,b)
-
+    '''
+    def mode(self,a,b):
+        try:
+            if(b==0):
+                raise Exception("Divisor Error")
+            
+        except Exception as e:
+            return e
         
+        return a%b
+    '''
